214808e1d8f286007f12b94395225534e2aac086
soumadip-cmd <soumadipsantra2004@gmail.com>
2024-11-15 22:14:00 +0530

    upfswt

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,12 +1,9 @@
 const mongoose = require("mongoose")
 
 
-async function connectDB(){
-    try{
-        await mongoose.connect(process.env.MONGODB_URI)
-    }catch(err){
-        console.log(err)
-    }
+ function connectDB(){
+    mongoose.connect(process.env.MONGODB_URI)
+    console.log("Database Connected..!")
 }
 
 module.exports = connectDB
