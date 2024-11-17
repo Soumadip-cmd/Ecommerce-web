60553624b52517dd035029ce9f51a42fb625f904
soumadip-cmd <soumadipsantra2004@gmail.com>
2024-11-17 18:08:41 +0530

    updated footer +responsive

--- frontend/src/pages/AllProducts.js
+++ frontend/src/pages/AllProducts.js
@@ -22,8 +22,8 @@ const AllProducts = () => {
   
   return (
     <div>
-        <div className='bg-white py-2 px-4 flex justify-between items-center'>
-            <h2 className='font-bold text-lg'>All Product</h2>
+        <div className='bg-white py-2 px-4 flex justify-end md:justify-between items-center'>
+            <h2 className='font-bold text-lg hidden md:flex '>All Product</h2>
             <button  className='border-2 border-red-600 text-red-600 hover:bg-red-600 hover:text-white transition-all py-1 px-3 rounded-full ' onClick={()=>setOpenUploadProduct(true)}>Upload Product</button>
         </div>
 
